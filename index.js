de7b94096beba9b95f442971219d27e47eae2497
mac <mac@macs-MacBook-Pro.local>
2025-01-08 17:22:28 +0500

    crud api

--- index.js
+++ index.js
@@ -1,15 +1,98 @@
-const express = require('express')
+const express = require('express');
+const { ClientSession } = require('mongodb');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js')
 
 const app = express()
-
-
-
+app.use(express.json());
 app.listen(3000, () =>{
     console.log('Server is running on 3000');
-}
-);
+});
+
+
+
+
 app.get('/', (req,res) => {
     res.send("Hello to the Node API and get uodated");
 });
-mongoose.connect("mongodb+srv://faisaliqbal1071:faisal1071@backend.f3ska.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backend")
+app.get('/api/getAll', async (req, res) => {
+    try{
+        const product = await Product.find();
+        res.status(200).json(product);
+    }
+    catch (error){
+        res.status(500).json({message: "error message", error});
+
+    }
+    
+
+});
+app.get('/api/findById/:id', async (req, res) => {
+    try{
+        const {id}= req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }
+    catch (error){
+        res.status(500).json({message: "error message", error});
+
+    }
+    
+
+});
+
+// update product
+app.put('/api/update/:id', async (req, res) => {
+    try{
+        const {id}= req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message:"Product not Found"});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    }
+    catch (error){
+        res.status(500).json({message: "error message", error});
+
+    }
+    
+
+});
+app.delete('/api/delete/:id', async (req, res) => {
+    try{
+        const {id}= req.params;
+        await Product.deleteOne({_id:id});
+        res.status(200).json({msg:"deleted successfully" });
+        
+    }
+    catch (error){
+        res.status(500).json({message: "error message", error});
+
+    }
+    
+
+});
+
+
+app.post('/api/products', async (req, res) => {
+        try{
+            const product = await Product.create(req.body);
+            res.status(200).json(product);
+        }
+        catch (error){
+            res.status(500).json({message: "error message", error});
+
+        }
+        
+
+});
+
+mongoose.connect("mongodb+srv://faisaliqbal1071:faisal1071@backend.f3ska.mongodb.net/?retryWrites=true&w=majority&appName=backend")
+    .then(()=> {
+console.log("Connect to the databse!");
+})
+    .catch(()=> {
+        console.log("Database Error");
+       
+    });
